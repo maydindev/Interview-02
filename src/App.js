6891e68b900c5d87605951666032465d4f424f57
maydindev <maydin.dev@outlook.com>
2024-08-09 15:51:42 +0300

    Init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+import { useState } from "react";
+
+function App() {
+  return <RobotList />;
+}
+
+const RobotList = () => {
+  // KODUNUZ BURAYA GELECEK
+};
+export default App;
